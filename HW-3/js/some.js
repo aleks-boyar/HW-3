d6c86a23c41761ac97cdc3de1aaed146b3770f16
aleks-boyar <79837040+aleks-boyar@users.noreply.github.com>
2021-05-05 13:40:47 +0300

    Add files via upload

---
+++ HW-3/js/some.js
@@ -0,0 +1,32 @@
+//Task#1
+// let user ={
+//    name:"John",
+//    surname:"Smith",
+
+// }
+// user.name="Pete";
+// delete user.name;
+
+//Task#2
+// const user={
+//    name:"John"
+// }
+// user.name='Pete';
+//да,это работает
+
+//Task#3
+// let salaries ={
+//    John:100,
+//    Ann:160,
+//    Pete:130
+// }
+// sum = (100+160+130);
+// console.log(sum);
+
+//Task#4
+// console.log(Boolean(Number("10")) + 1);//2
+// console.log(‘sub ’ + Number(false));//мне консоль выдала ошибку, но я думаю будет sub0
+// 16  *  ‘          91      ‘//1456
+// true-70//наверно -69
+Number(1 + String(1)) + 1//12?
+
